c1f16ef5d41046ec634883ee9c9cdfdab41cb58f
satyamcode <111707218+satyamcode@users.noreply.github.com>
2023-07-26 10:15:21 +0530

    Update server.js
    update1

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 // configure env
 dotenv.config();
 // rest object 
 //  database config
 connectDB();
 const app=express()
+ // esmodulefix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
 
 //  middlewares
 app.use(cors());
